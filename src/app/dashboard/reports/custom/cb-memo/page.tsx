2c8e5d6ac0066e2b03d26da5918c4408eb4318ac
sirwalterjones <walterjonesjr@gmail.com>
2025-05-01 13:50:34 +0000

    n

--- src/app/dashboard/reports/custom/cb-memo/page.tsx
+++ src/app/dashboard/reports/custom/cb-memo/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from "react";
 import { usePepiBooks } from "@/hooks/usePepiBooks";
-import { createClient } from "@/supabase/client";
+import { createClient } from "../../../../../supabase/client";
 import {
   getMonthlyPepiMemoDataAction,
   MonthlyPepiMemoData,
